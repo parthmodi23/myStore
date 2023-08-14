620edbf7a0edf4d24bfe958a9719fb90eb8bde1b
Parth Modi <parthmodi22500@gmail.com>
2023-08-14 21:29:50 +0530

    with offer

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -24,7 +24,7 @@ function Search() {
         {searchdata.map((searchvalue)=>(
             <div className="card" key={searchvalue.product_id}>
              <div className="image">  <Link to={`/clothesdetails/${searchvalue.product_id}`}> <img src={searchvalue.product_images} alt={searchvalue.product_name} />  </Link></div>
-                <h3 style={{textAlign:"left"}}>{searchvalue.product_name}</h3><br/>
+                <h3 style={{textAlign:"left"}}>{searchvalue.product_name}</h3>
                 <h3 style={{textAlign:"left"}}>${searchvalue.price}</h3>
                 <Link to={`/clothesdetails/${searchvalue.product_id}`}><button type="submit">details</button></Link>
 
