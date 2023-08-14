81dfc3a17c380331fd6d255d415a24a0f19cd34c
Parth Modi <parthmodi22500@gmail.com>
2023-08-14 22:19:16 +0530

    new

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -19,7 +19,7 @@ function SignIn() {
   const handleSignIn = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', credentials);
+      const response = await axios.post('https://lime-outrageous-codfish.cyclic.app/login', credentials);
 
       if (response && response.data) {
         // Successful sign-in
