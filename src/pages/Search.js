b8f573c62639082a934cd9b3aa5edede954e4603
Parth Modi <parthmodi22500@gmail.com>
2023-08-14 20:42:13 +0530

    with offer

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -26,7 +26,8 @@ function Search() {
              <div className="image">  <Link to={`/clothesdetails/${searchvalue.product_id}`}> <img src={searchvalue.product_images} alt={searchvalue.product_name} />  </Link></div>
                 <h3 style={{textAlign:"left"}}>{searchvalue.product_name}</h3><br/>
                 <h3 style={{textAlign:"left"}}>${searchvalue.price}</h3>
-              
+                <Link to={`/clothesdetails/${clothes.product_id}`}><button type="submit">details</button></Link>
+
             </div>
         ))}
 
